1403a4cc3b0f14dbb021c81c0a06b870b6abb4e7
NJ <78418423+NJeyyy@users.noreply.github.com>
2021-10-31 12:20:41 +0700

    Add @Description MetaBlock and other thing
    The other thing is just add function, to work only if the user not typing. If typing, the script will be ignored. If not, the script will run and do it business😎
    Oh yeah, and I also change the shortcut to " ` " only because I wanna test if it's work or not. If yes than Cool if not than shit :P

--- YT Scripts/Skip Ads shortcut.js
+++ YT Scripts/Skip Ads shortcut.js
@@ -4,13 +4,16 @@
 // @namespace      YT_scripts
 // @homepageURL    
 // @author         NJ1n9
-// @description    none for now..
+// @description    Skip Ads with shortcut!!
 // @include        https://www.youtube.com/watch?v=*
 // @icon           -
 // ==/UserScript==
 
 
-function doc_Keyup(Event) {
+function check () {
+  document.onkeyup = function(Event) {
+    evt = Event || window.event;
+       console.log(Event.target)
       var Skipads = document.getElementsByClassName("ytp-ad-skip-button");
       var HK1 = Event.which == 192
       var HK2 = Event.Ctrlkey
@@ -18,11 +21,15 @@ function doc_Keyup(Event) {
 // which == (unicodekey code) for not standalone hotkey
 // standalone hotkey list: "altKey", "MetaKey(winkey)", "CtrlKey", "shiftKey"
 // unicodekey code of hotkey can be found here -> https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_event_key_keycode2
-  if (HK1 && HK2 && HK3) {
-    if (Skipads.length == 1) { //Just to make sure the button exist:P
-          Skipads[0].click();
-    }
-  }
-};
-//register the handler
-document.addEventListener('keyup', doc_Keyup, false);
+       if (HK1) {
+         if (!(Event.target instanceof HTMLTextAreaElement) && !(Event.target instanceof HTMLInputElement)) {
+         // Your commands after the shortcut here
+            if (Skipads.length == 1) { //Just to make sure the button exist:P
+                Skipads[0].click();
+              }
+           }
+        }  
+    };
+}
+    
+check()
