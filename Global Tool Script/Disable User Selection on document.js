aaf1d0d98d77383356362f1807b58f616b334946
NJ <78418423+NJeyyy@users.noreply.github.com>
2022-01-05 00:23:29 +0700

    Update Disable User Selection on document.js

--- Global Tool Script/Disable User Selection on document.js
+++ Global Tool Script/Disable User Selection on document.js
@@ -14,12 +14,11 @@ let createCSS_ElementToggleBTN_DUS = document.createElement("style")
 createCSS_ElementToggleBTN_DUS.setAttribute("id", "CSS_DisableUserSelection");
 createCSS_ElementToggleBTN_DUS.innerHTML = 'div.DisableUserSelection {\n' + ' position: fixed;\n' + ' width: 30px;\n' + ' display: block;\n' + ' height: 30px;\n' + ' background: #f1f1f1;\n' + ' bottom: 10px;\n' + ' right: 10px;\n' + ' font-size: 30px;\n' + ' border-radius: 50%;\n' + ' user-select: none;' + ' opacity: 85%;' + '}\n' + '\n' + 'div.DisableUserSelection[ToggleStatus="ON"] {\n' + ' color: red;\n' + ' cursor: url(https://cdn.custom-cursor.com/db/cursor/32/Rock_Hand_cursor.png), auto;' + '}\n' + 'div.DisableUserSelection[ToggleStatus="ON"]:not(:hover) {\n' + ' opacity: 65%;\n' + '}\n' + '\n' + 'div.DisableUserSelection[ToggleStatus="OFF"] {\n' + ' color: green;\n' + ' cursor: alias;\n' + '}\n' + 'div.DisableUserSelection[ToggleStatus="OFF"]:not(:hover, :active) {\n' + ' opacity: 20%;\n' + '}\n';
 document.head.appendChild(createCSS_ElementToggleBTN_DUS)
-
 let create_ElementToggleBTN_DUS = document.createElement("div")
-create_ElementToggleBTN_DUS.setAttribute("class", "DisableUserSelection fas fa-check-circle");
+create_ElementToggleBTN_DUS.setAttribute("class", "DisableUserSelection fas");
 create_ElementToggleBTN_DUS.setAttribute("ToggleStatus", "OFF");
 document.body.appendChild(create_ElementToggleBTN_DUS)
-document.querySelectorAll("div.DisableUserSelection")[0].addEventListener("click", Userselection_disabled)
+
 
 
 function Userselection_disabled() {
@@ -49,4 +48,23 @@ function Userselection_enabled() {
   document.querySelectorAll("style.DisableUserSelection")[0].remove();
   document.querySelectorAll("div.DisableUserSelection")[0].removeEventListener("click", Userselection_enabled)
   document.querySelectorAll("div.DisableUserSelection")[0].addEventListener("click", Userselection_disabled)
+}
+
+
+//TOGGLE CONDITION
+let TOGGLEONOFF = "OFF"
+// ON means the user selection is disabled on page when this script runs.
+// OFF means the user selection is enabled and disabled manually by button.
+
+if (TOGGLEONOFF = "ON") {
+  let divDsbld = document.querySelectorAll("div.DisableUserSelection")[0]
+  divDsbld.setAttribute("ToggleStatus", "ON");
+  divDsbld.addEventListener("click", Userselection_enabled);
+  divDsbld.classList.add("fa-check-circle");
+}
+else if (TOGGLEONOFF = "OFF") {
+  let divDsbld = document.querySelectorAll("div.DisableUserSelection")[0]
+  divDsbld.setAttribute("ToggleStatus", "OFF");
+  divDsbld.addEventListener("click", Userselection_disabled);
+  divDsbld.classList.add("fa-times-circle");
 }
