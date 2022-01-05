72bf940487876164a0bb57d813412037a88a9930
NJ <78418423+NJeyyy@users.noreply.github.com>
2022-01-05 13:20:29 +0700

    Fix the "if"/"else if" condition error
    WHY TF.. IT'S.. ["Condition1 = "Condition2"] AND NOT ["Condition1 == "Condition2"]
    AAAAAAAAAAAAAARRRGGGGHHHHHHHHHHHH

--- Global Tool Script/Disable User Selection on document.js
+++ Global Tool Script/Disable User Selection on document.js
@@ -22,13 +22,12 @@ justforawait()
 
 async function justforawait() {
 await waitFor(_ => document.getElementById("FontAwesome5script"))
-let createCSS_ElementToggleBTN_DUS = document.createElement("style")
+let createCSS_ElementToggleBTN_DUS = document.createElement("style");
 createCSS_ElementToggleBTN_DUS.setAttribute("id", "CSS_DisableUserSelection");
 createCSS_ElementToggleBTN_DUS.innerHTML = 'div.DisableUserSelection {\n' + ' position: fixed;\n' + ' width: 30px;\n' + ' display: block;\n' + ' height: 30px;\n' + ' background: #f1f1f1;\n' + ' bottom: 10px;\n' + ' right: 10px;\n' + ' font-size: 30px;\n' + ' border-radius: 50%;\n' + ' user-select: none;' + ' opacity: 85%;' + '}\n' + '\n' + 'div.DisableUserSelection[ToggleStatus="ON"] {\n' + ' color: red;\n' + ' cursor: url(https://cdn.custom-cursor.com/db/cursor/32/Rock_Hand_cursor.png), auto;' + '}\n' + 'div.DisableUserSelection[ToggleStatus="ON"]:not(:hover) {\n' + ' opacity: 65%;\n' + '}\n' + '\n' + 'div.DisableUserSelection[ToggleStatus="OFF"] {\n' + ' color: green;\n' + ' cursor: alias;\n' + '}\n' + 'div.DisableUserSelection[ToggleStatus="OFF"]:not(:hover, :active) {\n' + ' opacity: 20%;\n' + '}\n';
 document.head.appendChild(createCSS_ElementToggleBTN_DUS)
-let create_ElementToggleBTN_DUS = document.createElement("div")
+let create_ElementToggleBTN_DUS = document.createElement("div");
 create_ElementToggleBTN_DUS.setAttribute("class", "DisableUserSelection fas");
-create_ElementToggleBTN_DUS.setAttribute("ToggleStatus", "OFF");
 document.body.appendChild(create_ElementToggleBTN_DUS)
 
 
@@ -37,7 +36,7 @@ function Userselection_disabled() {
   document.querySelectorAll("div.DisableUserSelection")[0].classList.replace("fa-check-circle", "fa-times-circle")
   document.querySelectorAll("div.DisableUserSelection")[0].setAttribute("ToggleStatus", "ON");
   
-  var DisableUserselection = document.createElement("style")
+  let DisableUserselection = document.createElement("style")
   DisableUserselection.setAttribute("class", "DisableUserSelection")
   DisableUserselection.innerHTML = "html {\n"
     + "user-select: none;\n"
@@ -66,18 +65,23 @@ function Userselection_enabled() {
 
 
 //TOGGLE CONDITION
-let TOGGLEONOFF = "OFF"
+var TOGGLEONOFF = "OFF"
 // ON means the user selection is disabled on page when this script runs.
 // OFF means the user selection is enabled and disabled manually by button.
 
-if (TOGGLEONOFF = "ON") {
-  let divDsbld = document.querySelectorAll("div.DisableUserSelection")[0]
+if (TOGGLEONOFF == "ON") {
+  let DisableUserselection = document.createElement("style")
+  DisableUserselection.setAttribute("class", "DisableUserSelection")
+  DisableUserselection.innerHTML = "html {\n" + "user-select: none;\n" + "}\n" + "\n" + "input, textarea {\n" + " user-select: text;\n" + "}\n";
+  document.head.appendChild(DisableUserselection)
+  
+  let divDsbld = document.querySelectorAll("div.DisableUserSelection")[0];
   divDsbld.setAttribute("ToggleStatus", "ON");
   divDsbld.addEventListener("click", Userselection_enabled);
   divDsbld.classList.add("fa-times-circle");
   divDsbld.setAttribute("title", "You cannot selecting things\nSince this turned ON.")
 }
-else if (TOGGLEONOFF = "OFF") {
+else if (TOGGLEONOFF == "OFF") {
   let divDsbld = document.querySelectorAll("div.DisableUserSelection")[0]
   divDsbld.setAttribute("ToggleStatus", "OFF");
   divDsbld.addEventListener("click", Userselection_disabled);
