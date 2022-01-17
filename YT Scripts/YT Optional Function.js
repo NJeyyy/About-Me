d079bef280b6cc6734666f80a70f94427c54760a
NJ <78418423+NJeyyy@users.noreply.github.com>
2022-01-17 16:20:40 +0700

    A little changes too

--- YT Scripts/YT Optional Function.js
+++ YT Scripts/YT Optional Function.js
@@ -1,4 +1,4 @@
-/* JUST SAVING IT HERE.. WHO KNOW I WILL LOST IT RIGHT? */
+/* JUST SAVING IT HERE.. WHO KNOWs I WILL LOST IT RIGHT? */
 
 // ==UserScript==
 // @name           YT Optional Function
@@ -48,164 +48,192 @@ Change YouTube Website theme based on time: Does what it say, It change when mat
  * => AlreadyRUN_YTOptionalFunction3
  *  Temporary var
 */
+sessionStorage.removeItem("gotoCommentIT");
 sessionStorage.setItem("AlreadyRUN_YTOptionalFunction1", false)
 sessionStorage.setItem("AlreadyRUN_YTOptionalFunction2", false)
 
 start_YTOptionalFunction()
 async function start_YTOptionalFunction() {
-let LoadingOverlayCSS = document.createElement("style")
-LoadingOverlayCSS.setAttribute("class", "LoadingOverlayELEMENTS")  
-LoadingOverlayCSS.innerHTML = 'div[name="LoadingOverlay"] {\n'
-  + ' background-color: black;\n'
-  + ' top: 0px;\n'
-  + ' left: 0px;\n'
-  + ' width: 100vw;\n'
-  + ' height: 100vh;\n'
-  + ' z-index: 99999;\n'
-  + ' opacity: 80%;\n'
-  + ' position: fixed;\n'
-  + ' cursor: url(https://i.ibb.co/qgPfw89/m5p-FZPd-RESIZED.png), auto;\n'
-  + ' user-select: none;'
-  + '}\n'
-  + 'img[name="LoadingIcon"] {\n'
-  + ' filter: invert(86%) sepia(1%) saturate(1660%) hue-rotate(115deg) brightness(107%) contrast(113%);\n'
-  + ' height: auto;\n'
-  + ' width: 10vw;\n'
-  + ' position: absolute;\n'
-  + ' top: 50vh;\n'
-  + ' left: 50vw;\n'
-  + ' transform: translate(-50%,-50%);\n'
-  + '}\n'
-;
-document.head.appendChild(LoadingOverlayCSS)
-let LoadingOverlayDIV = document.createElement("div")
-LoadingOverlayDIV.setAttribute("class", "LoadingOverlayELEMENTS")
-LoadingOverlayDIV.setAttribute("name", "LoadingOverlay")
-LoadingOverlayDIV.innerHTML = '<img name="LoadingIcon" src="https://i0.wp.com/mdhsociety.com/wp-content/uploads/2017/11/throbber.gif">'
-document.body.appendChild(LoadingOverlayDIV)
-document.getElementsByName('LoadingIcon')[0].setAttribute('draggable', false);
-document.getElementsByName('LoadingOverlay')[0].addEventListener("contextmenu", (event) => {
-  event.preventDefault();
-  let CFM = confirm("Do you want to remove the ['𝘌𝘭𝘦𝘮𝘦𝘯𝘵.𝙻𝚘𝚊𝚍𝚒𝚗𝚐𝙾𝚟𝚎𝚛𝚕𝚊𝚢𝙴𝙻𝙴𝙼𝙴𝙽𝚃𝚂'].\nTo ignore the loading and just do whatever U want?")
-  if (CFM == true) {
-    document.getElementsByClassName("LoadingOverlayELEMENTS")[1].remove();
-    document.getElementsByClassName("LoadingOverlayELEMENTS")[0].remove();
+  var BlockVideo2Play;
+  if (sessionStorage.getItem("EnterCommentKEY")) {
+    sessionStorage.removeItem("EnterCommentKEY")
   }
-});
-if (window.location.href.match("https://www.youtube.com/watch*")) {
-  var BlockVideo2Play = setInterval(BLOCKAUTOPLAY, 1000)
-}
-var THEMETIMERL = setInterval(Theme_BasedTime, 60 * 60000) // Change YouTube Website theme based on time (make it runs on time of interval)
-await sleep(700)
-await ToggleTHEMEBTN();
-await Startonlyonvideopage() // START ONLY ON VIDEO PAGE
-await sleep(1000)
-await Theme_BasedTime()
-await sleep(500)
-if (document.getElementsByClassName("LoadingOverlayELEMENTS").length > 0) {
-   document.getElementsByClassName("LoadingOverlayELEMENTS")[1].remove();
-   document.getElementsByClassName("LoadingOverlayELEMENTS")[0].remove();
-}
-if (window.location.href.match("https://www.youtube.com/watch*")) {
-  await sleep(1500)
-  // "true" to make it go to 0:00 Automatically
-  // "[Insert any words to disable it(or just 'false')]" it's not gonna ask or automatically go to 0:00
-  // "ask" It ask wether it wanna go to 0:00 or not
-  let GOTOZERO = "false" // ALL MUST LOWER CASE
-  if (GOTOZERO == "ask") {
-    let YESNO = confirm("Go to [0:00]? ")
-    if (YESNO == true) {
+  if (window.location.href.match("https://www.youtube.com/watch*")) {
+    let LoadingOverlayCSS = document.createElement("style")
+    LoadingOverlayCSS.setAttribute("class", "LoadingOverlayELEMENTS")  
+    LoadingOverlayCSS.innerHTML = 'div[name="LoadingOverlay"] {\n'
+      + ' background-color: black;\n'
+      + ' top: 0px;\n'
+      + ' left: 0px;\n'
+      + ' width: 100vw;\n'
+      + ' height: 100vh;\n'
+      + ' z-index: 99999;\n'
+      + ' opacity: 80%;\n'
+      + ' position: fixed;\n'
+      + ' cursor: url(https://i.ibb.co/qgPfw89/m5p-FZPd-RESIZED.png), auto;\n'
+      + ' user-select: none;\n'
+      + ' pointer-events: none;\n'
+      + '}\n'
+      + 'img[name="LoadingIcon"] {\n'
+      + ' filter: invert(86%) sepia(1%) saturate(1660%) hue-rotate(115deg) brightness(107%) contrast(113%);\n'
+      + ' height: auto;\n'
+      + ' width: 10vw;\n'
+      + ' position: absolute;\n'
+      + ' top: 50vh;\n'
+      + ' left: 50vw;\n'
+      + ' transform: translate(-50%,-50%);\n'
+      + ' pointer-events: auto;\n'
+      + '}\n'
+    ;
+    document.head.appendChild(LoadingOverlayCSS)
+    let LoadingOverlayDIV = document.createElement("div")
+    LoadingOverlayDIV.setAttribute("class", "LoadingOverlayELEMENTS")
+    LoadingOverlayDIV.setAttribute("name", "LoadingOverlay")
+    LoadingOverlayDIV.innerHTML = '<img name="LoadingIcon" title="Right click here to remove this overlay!" src="https://i0.wp.com/mdhsociety.com/wp-content/uploads/2017/11/throbber.gif">'
+    document.body.appendChild(LoadingOverlayDIV)
+    document.getElementsByName('LoadingIcon')[0].setAttribute('draggable', false);
+    document.getElementsByName('LoadingIcon')[0].addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      let CFM = confirm("Do you want to remove the ['𝘌𝘭𝘦𝘮𝘦𝘯𝘵.𝙻𝚘𝚊𝚍𝚒𝚗𝚐𝙾𝚟𝚎𝚛𝚕𝚊𝚢𝙴𝙻𝙴𝙼𝙴𝙽𝚃𝚂'].\nTo ignore the loading and just do whatever U want?")
+      if (CFM == true) {
+        document.getElementsByClassName("LoadingOverlayELEMENTS")[1].remove();
+        document.getElementsByClassName("LoadingOverlayELEMENTS")[0].remove();
+      }
+    });
+    BlockVideo2Play = setInterval(BLOCKAUTOPLAY, 1000)
+  }
+  var THEMETIMERL = setInterval(Theme_BasedTime, 60 * 60000) // Change YouTube Website theme based on time (make it runs on time of interval)
+  await sleep(700)
+  await ToggleTHEMEBTN();
+  await Theme_BasedTime();
+  await Startonlyonvideopage() // START ONLY ON VIDEO PAGE
+  await sleep(500)
+  if (window.location.href.match("https://www.youtube.com/watch*")) {
+    await sleep(900)
+    // "true" to make it go to 0:00 Automatically
+    // "[Insert any words to disable it(or just 'false')]" it's not gonna ask or automatically go to 0:00
+    // "ask" It ask wether it wanna go to 0:00 or not
+    let GOTOZERO = "false" // ALL MUST LOWER CASE
+    if (GOTOZERO == "ask") {
+      let YESNO = confirm("Go to [0:00]? ")
+      if (YESNO == true) {
+        document.getElementById("movie_player").seekTo(0,true);
+      }
+    } else if (GOTOZERO == "true") {
       document.getElementById("movie_player").seekTo(0,true);
     }
+    if (document.getElementsByClassName("LoadingOverlayELEMENTS").length > 0) {
+      document.getElementsByClassName("LoadingOverlayELEMENTS")[1].remove();
+      document.getElementsByClassName("LoadingOverlayELEMENTS")[0].remove();
+    }
   }
-  else if (GOTOZERO == "true") {
-    document.getElementById("movie_player").seekTo(0,true);
+  if (BlockVideo2Play) {
+    clearInterval(BlockVideo2Play);
   }
-  clearInterval(BlockVideo2Play)
-}
 }
 
 
 // Start whenever page changed the URL
 window.addEventListener('locationchange', async function() {
   // Put your code here
-  await sleep(2000)
-  let LoadingOverlayCSS = document.createElement("style")
-LoadingOverlayCSS.setAttribute("class", "LoadingOverlayELEMENTS")  
-LoadingOverlayCSS.innerHTML = 'div[name="LoadingOverlay"] {\n'
-  + ' background-color: black;\n'
-  + ' top: 0px;\n'
-  + ' left: 0px;\n'
-  + ' width: 100vw;\n'
-  + ' height: 100vh;\n'
-  + ' z-index: 99999;\n'
-  + ' opacity: 80%;\n'
-  + ' position: fixed;\n'
-  + ' cursor: url(https://i.ibb.co/qgPfw89/m5p-FZPd-RESIZED.png), auto;\n'
-  + ' user-select: none;'
-  + '}\n'
-  + 'img[name="LoadingIcon"] {\n'
-  + ' filter: invert(86%) sepia(1%) saturate(1660%) hue-rotate(115deg) brightness(107%) contrast(113%);\n'
-  + ' height: auto;\n'
-  + ' width: 10vw;\n'
-  + ' position: absolute;\n'
-  + ' top: 50vh;\n'
-  + ' left: 50vw;\n'
-  + ' transform: translate(-50%,-50%);\n'
-  + '}\n'
-;
-document.head.appendChild(LoadingOverlayCSS)
-let LoadingOverlayDIV = document.createElement("div")
-LoadingOverlayDIV.setAttribute("class", "LoadingOverlayELEMENTS")
-LoadingOverlayDIV.setAttribute("name", "LoadingOverlay")
-LoadingOverlayDIV.innerHTML = '<img name="LoadingIcon" src="https://i0.wp.com/mdhsociety.com/wp-content/uploads/2017/11/throbber.gif">'
-document.body.appendChild(LoadingOverlayDIV)
-  await sleep(500)
-  if (window.location.href.match("https://www.youtube.com/watch*")) {
-    var BlockVideo2Play = setInterval(BLOCKAUTOPLAY, 1000)
+  await sleep(1500)
+  var BlockVideo2Play;
+  if (sessionStorage.getItem("EnterCommentKEY")) {
+    sessionStorage.removeItem("EnterCommentKEY")
   }
-  await Startonlyonvideopage()
-  await Theme_BasedTime()
   if (window.location.href.match("https://www.youtube.com/watch*")) {
-    await sleep(1500)
-  // "true" to make it go to 0:00 Automatically
-  // "[Insert any words to disable it(or just 'false')]" it's not gonna ask or automatically go to 0:00
-  // "ask" It ask wether it wanna go to 0:00 or not
-  let GOTOZERO = "false" // ALL MUST be LOWER CASE
-  if (GOTOZERO == "ask") {
-    let YESNO = confirm("Go to [0:00]? ")
-    if (YESNO == true) {
+    let LoadingOverlayCSS = document.createElement("style")
+    LoadingOverlayCSS.setAttribute("class", "LoadingOverlayELEMENTS")  
+    LoadingOverlayCSS.innerHTML = 'div[name="LoadingOverlay"] {\n'
+      + ' background-color: black;\n'
+      + ' top: 0px;\n'
+      + ' left: 0px;\n'
+      + ' width: 100vw;\n'
+      + ' height: 100vh;\n'
+      + ' z-index: 99999;\n'
+      + ' opacity: 70%;\n'
+      + ' position: fixed;\n'
+      + ' cursor: url(https://i.ibb.co/qgPfw89/m5p-FZPd-RESIZED.png), auto;\n'
+      + ' user-select: none;\n'
+      + ' pointer-events: none;\n'
+      + '}\n'
+      + 'img[name="LoadingIcon"] {\n'
+      + ' filter: invert(86%) sepia(1%) saturate(1660%) hue-rotate(115deg) brightness(107%) contrast(113%);\n'
+      + ' height: auto;\n'
+      + ' width: 10vw;\n'
+      + ' position: absolute;\n'
+      + ' top: 50vh;\n'
+      + ' left: 50vw;\n'
+      + ' transform: translate(-50%,-50%);\n'
+      + ' pointer-events: auto;\n'
+      + '}\n'
+    ;
+    document.head.appendChild(LoadingOverlayCSS)
+    let LoadingOverlayDIV = document.createElement("div")
+    LoadingOverlayDIV.setAttribute("class", "LoadingOverlayELEMENTS")
+    LoadingOverlayDIV.setAttribute("name", "LoadingOverlay")
+    LoadingOverlayDIV.innerHTML = '<img name="LoadingIcon" title="Right click here to remove this overlay!" src="https://i0.wp.com/mdhsociety.com/wp-content/uploads/2017/11/throbber.gif">'
+    document.body.appendChild(LoadingOverlayDIV)
+    document.getElementsByName('LoadingIcon')[0].setAttribute('draggable', false);
+    document.getElementsByName('LoadingIcon')[0].addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      let CFM = confirm("Do you want to remove the ['𝘌𝘭𝘦𝘮𝘦𝘯𝘵.𝙻𝚘𝚊𝚍𝚒𝚗𝚐𝙾𝚟𝚎𝚛𝚕𝚊𝚢𝙴𝙻𝙴𝙼𝙴𝙽𝚃𝚂'].\nTo ignore the loading and just do whatever U want?")
+      if (CFM == true) {
+        document.getElementsByClassName("LoadingOverlayELEMENTS")[1].remove();
+        document.getElementsByClassName("LoadingOverlayELEMENTS")[0].remove();
+      }
+    });
+    BlockVideo2Play = setInterval(BLOCKAUTOPLAY, 1000);
+  }
+  await sleep(500);
+  await Startonlyonvideopage();
+  await Theme_BasedTime();
+  if (window.location.href.match("https://www.youtube.com/watch*")) {
+    await sleep(900)
+    // "true" to make it go to 0:00 Automatically
+    // "[Insert any words to disable it(or just 'false')]" it's not gonna ask or automatically go to 0:00
+    // "ask" It ask wether it wanna go to 0:00 or not
+    let GOTOZERO = "false" // ALL MUST be LOWER CASE
+    if (GOTOZERO == "ask") {
+      let YESNO = confirm("Go to [0:00]? ")
+      if (YESNO == true) {
+        document.getElementById("movie_player").seekTo(0,true);
+      }
+    } else if (GOTOZERO == "true") {
       document.getElementById("movie_player").seekTo(0,true);
     }
   }
-  else if (GOTOZERO == "true") {
-    document.getElementById("movie_player").seekTo(0,true);
-  }
+  if (BlockVideo2Play) {
     clearInterval(BlockVideo2Play)
   }
-  await sleep(500)
+  await sleep(600)
   if (document.getElementsByClassName("LoadingOverlayELEMENTS").length > 0) {
-   document.getElementsByClassName("LoadingOverlayELEMENTS")[1].remove();
-   document.getElementsByClassName("LoadingOverlayELEMENTS")[0].remove();
-}
+    document.getElementsByClassName("LoadingOverlayELEMENTS")[1].remove();
+    document.getElementsByClassName("LoadingOverlayELEMENTS")[0].remove();
+  }
   console.log("YT Optional Function")
-
-})
+});
 
 
 // START ONLY ON VIDEO PAGE
 async function Startonlyonvideopage() {
-  await sleep(5000)
+  await sleep(2500)
   var URLRequirements = window.location.href
   var UsedURL = new RegExp("https://www.youtube.com/watch*")
   
   if (sessionStorage.getItem("AlreadyRUN_YTOptionalFunction1") == "true") {
     window.removeEventListener('keyup', YTAds_Shortcut, false);
+    window.removeEventListener("click", EnterCommentKEY)
+    window.removeEventListener("keyup", gotoCommentIT, false)
     sessionStorage.setItem("AlreadyRUN_YTOptionalFunction1", false)
   }
   if (URLRequirements.match(UsedURL)) {
     window.addEventListener('keyup', YTAds_Shortcut, false); // Skip Ads with shortcut (`/~)
+    window.addEventListener("keyup", EnterCommentKEYFunc); // Enter to submit comment
+    window.addEventListener("keyup", gotoCommentIT) // <-- alt + C to comment
     sessionStorage.setItem("AlreadyRUN_YTOptionalFunction1", true)
+    window.dispatchEvent(new Event("scroll"));
+    document.dispatchEvent(new Event("scroll"));
   }
   else {
     console.log("This script is not working here =w=")
@@ -215,7 +243,6 @@ async function Startonlyonvideopage() {
 //=================== Skip Ads with shortcut (`/~) ========================================
 /**/
 function YTAds_Shortcut (Event) {
-    evt = Event || window.event;
     
     //VARIABLES//
     var Skipads = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
@@ -275,7 +302,7 @@ async function Theme_BasedTime() {
   // --The selected theme settings--
   /* "Dark" for dark theme and "Light" for light theme. Be careful on the capital! It's important. */
   var theme_SELECTIONONE = "Dark" // NIGHT TIME
-  var theme_SELECTIONTWO = "Light" // DAY TIME
+  var theme_SELECTIONTWO = "Dark" // DAY TIME
   
   // <-- Use "||" because it's more to "or" than "and" and also because it's PM to AM combined.. not AM to PM:P
   if (timeOfDay >= time_NIGHT1 || timeOfDay <= time_NIGHT2) {  //<-- NIGHT TIME [Dark Theme]
@@ -434,16 +461,43 @@ else if (!(Element_HTML.getAttribute("dark") == "true")) { // When Current theme
 //```````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````\\
 //=================== Block Video Play on Loading ========================================
 /**/
+//let CRT_menuelem = document.createElement("div");
+//CRT_menuelem.setAttribute("id", "PlayPause_controlmenu");
+//document.body.appendChild(CRT_menuelem);
+//let 2Pmenuelem = document.getElementsByClassName("ytp-play-button")[0];
+//2Pmenuelem.addEventListener("contextmenu", (evt) => {evt.preventDefault();//Togglething})
 function BLOCKAUTOPLAY() {
-  var vids = document.getElementsByClassName("video-stream");
+  let vids = document.getElementsByClassName("video-stream");
   if (vids.length) { // this is basically to make sure there are videos on the page, but it is probably unnecessary
-    var Controls = document.getElementsByClassName("ytp-play-button");
-    var Status = Controls[0].getAttribute("aria-label");
-    
-    if (!(Status == "Play (k)")) { //if the video is playing, status comes back as "Pause"
-      Controls[0].click(); // in which case we want to click on the play-pause button to pause it
-    }
+    document.querySelector("video.video-stream").pause(); // pause the vid. JUST THAT!
   }
 }
 //*/
+//```````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````\\
+//=================== Press enter to Comment Instead of clicking + Shortcut to comment ========================================
+// unicodekey code of hotkey can be found here -> https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_event_key_keycode2
+function EnterCommentKEYFunc(E) {
+  E.preventDefault();
+  let ECKEYHk1 = E.which == 13 // <-- EnterKey
+  let ECKEYHk2 = E.shiftKey
+  if (!(ECKEYHk2) && ECKEYHk1 && (document.activeElement == document.getElementsByClassName("yt-formatted-string")[9])) {
+    document.getElementsByClassName("ytd-commentbox")[35].click();
+    sessionStorage.removeItem("EnterCommentKEY");
+  }
+}
+
+async function gotoCommentIT(E) {
+  await sleep(700)
+  let gtCommentHk1 = E.which == 67 // <-- C
+  let gtCommentHk2 = E.altKey
+  if (gtCommentHk2 && gtCommentHk1) {
+      sessionStorage.setItem("gotoCommentIT", "true");
+      if ((document.getElementsByClassName("style-scope ytd-comment-simplebox-renderer").length == 7) && (document.activeElement !== document.getElementsByClassName("yt-formatted-string")[9])) {
+        document.getElementsByClassName("style-scope ytd-comment-simplebox-renderer")[1].click();
+      } else if (document.activeElement == document.getElementsByClassName("yt-formatted-string")[9]) {
+        document.activeElement.blur()
+        window.scrollTo(0,0);
+      }
+  }
+}
 //```````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````\\
