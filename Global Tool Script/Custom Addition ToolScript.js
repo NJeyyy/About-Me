fa3b4373fb5683060a9ee9a419dac53d4fa432d9
NJ <78418423+NJeyyy@users.noreply.github.com>
2022-01-21 15:56:27 +0700

    Small Updates

--- Global Tool Script/Custom Addition ToolScript.js
+++ Global Tool Script/Custom Addition ToolScript.js
@@ -4,7 +4,6 @@
 // @match           *://*/*
 // @version         1.5
 // @author          NJ1n9
-// @run-at          document-start
 // @description     Some ToolScript that combined.
 // @icon            https://cdn.iconscout.com/icon/free/png-256/code-336-830581.png
 // @noframes
@@ -23,6 +22,7 @@
         The one that not "document-start" or before that;D
  * 
  */
+/**
 // ToolScript 1
 //===========SLEEP(miliseconds) Function================================================================\\  <-- Added on V1
 // Source: https://stackoverflow.com/a/39914235/15715476
@@ -70,28 +70,82 @@ function evil(fn) {
   return new Function('return ' + fn)();
 }
 //```````````````````````````````````````````````````````````````````````````````````````````````````````````\\
+// ToolScript 4
+//===========Copy To Clipboard================================================================\\  <-- Added on V1.5
+// Source: https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+// Copy Text. or things, to clipboard.
+
+function copyToClipboard(text) {
+   const elem = document.createElement('textarea');
+   elem.value = text;
+   document.body.appendChild(elem);
+   elem.select();
+   document.execCommand('copy');
+   document.body.removeChild(elem);
+}
+//```````````````````````````````````````````````````````````````````````````````````````````````````````````\\
 //*/
 
 
 
 //~~~~~~~~~~~Implement in to the "head" tag so it can be used globally~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\ <-- Added and fixed in v1.5
 /**/
-Custom_Addition_ToolScript()
-function Custom_Addition_ToolScript() {
+Custom_Addition_ToolScript();
+AddonsScript();
+async function Custom_Addition_ToolScript() {
   // ToolScript1-- SLEEP(miliseconds) Function
   let ToolScript1 = "function sleep(ms) {\n" + "  return new Promise(resolve => setTimeout(resolve, ms));\n" + "};\n";
   // ToolScript2-- WaitFor a conditionFunction
   let ToolScript2 = "function waitFor(conditionFunction) {\n" + "\n" + "  const poll = resolve => {\n" + "    if(conditionFunction()) resolve();\n" + "    else setTimeout(_ => poll(resolve), 400);\n" + "  }\n" + "\n" + "  return new Promise(poll);\n" + "}\n";
   // ToolScript3-- Alternate to Eval()
   let ToolScript3 = "function evil(fn) {\n" + "  return new Function('return ' + fn)();\n" + "}\n";
+  // ToolScript4-- Copy To Clipboard
+  let ToolScript4 = 'function copyToClipboard(text) {\n' + '   const elem = document.createElement("textarea");\n' + '   elem.value = text;\n' + '   document.body.appendChild(elem);\n' + '   elem.select();\n' + '   document.execCommand("copy");\n' + '   document.body.removeChild(elem);\n' + '}\n';
   
   let ToolScriptSeparator = "\n//----------------------------------\n\n"
-  let Scriptof_CustomAdditionToolScript = ToolScript1 + ToolScriptSeparator + ToolScript2 + ToolScriptSeparator + ToolScript3;
+  let Scriptof_CustomAdditionToolScript = ToolScript1 + ToolScriptSeparator + ToolScript2 + ToolScriptSeparator + ToolScript3 + ToolScriptSeparator + ToolScript4;
+  let consolethng = "%c" + "Custom ToolScript is Added." + "\n\nHere's the list of added command:" + "\n>waitFor(conditionFunction)\n  To wait for a certain condition to met." + "\n>sleep(ms)\n  It is useful to wait for certain task." + "\n>evil(fn)\n  For Counting a string. Similar to Eval() I.. literally don't know what's the difference rn btw..";
   let Create_CustomAdditionToolScriptElem = document.createElement("script");
   Create_CustomAdditionToolScriptElem.setAttribute("id", "Custom-Addition-ToolScript")
   Create_CustomAdditionToolScriptElem.setAttribute("type", "application/javascript")
   Create_CustomAdditionToolScriptElem.innerHTML = Scriptof_CustomAdditionToolScript;
-  document.head.appendChild(Create_CustomAdditionToolScriptElem)
+  await document.head.appendChild(Create_CustomAdditionToolScriptElem)
+  console.log(consolethng, "color: blue;font-family: monospace;");
+}
+
+async function AddonsScript() {
+  var TheScriptElem = document.getElementById("Custom-Addition-ToolScript");
+  var CurrentHost = window.location.hostname;
+  //Host list, Only add this script on specific domain/host.
+  var YT = "www.youtube.com"
+  
+  if (CurrentHost == YT) {
+    let YT_AddonS1_varqup1 = /^https?\:\/\/(?:www\.youtube(?:\-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*\&)?vi?=|\&vi?=|\?(?:.*\&)?vi?=)([^#\&\?\n\/<>"]*)/i
+    let YT_AddonS1 = "function GetYoutubeVid_id(url){\n" + ' var regExp = ' + YT_AddonS1_varqup1 + ';\n' + ' var match = url.match(regExp);\n' + ' return (match && match[1].length==11)? match[1] : false;\n' + '}\n'
+    let YT_Addon = "\n" + YT_AddonS1;
+    
+    let CreateAddition_CustomAdditionToolScriptElem = document.createElement("script");
+    CreateAddition_CustomAdditionToolScriptElem.setAttribute("class", "Custom-Addition-ToolScript_Optional");
+    CreateAddition_CustomAdditionToolScriptElem.setAttribute("type", "application/javascript");
+    CreateAddition_CustomAdditionToolScriptElem.innerHTML = YT_Addon;
+    await document.head.appendChild(CreateAddition_CustomAdditionToolScriptElem);
+    let consolethng = "%c" + "Another Command:\n" + ">GetYoutubeVid_id(url)\n  Get YouTube Video id!";
+    console.log(consolethng, "color: navy;font-family: monospace;");
+  }
 }
 //*/
 //###############################################################################################################\\
+/**
+function GetYoutubeVid_id(url){
+ var regExp = /^https?\:\/\/(?:www\.youtube(?:\-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*\&)?vi?=|\&vi?=|\?(?:.*\&)?vi?=)([^#\&\?\n\/<>"]*)/i;
+ var match = url.match(regExp);
+ return (match && match[1].length==11)? match[1] : false;
+}
+*/
+
+
+
+
+
+
+//=======================================================================================
