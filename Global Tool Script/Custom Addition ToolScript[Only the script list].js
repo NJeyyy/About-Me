dd58f08d979f095a98dfa464d8b0896cbb2528d6
NJ <78418423+NJeyyy@users.noreply.github.com>
2022-02-26 20:24:25 +0700

    add 2 new code aka toolscript

--- Global Tool Script/Custom Addition ToolScript[Only the script list].js
+++ Global Tool Script/Custom Addition ToolScript[Only the script list].js
@@ -91,4 +91,23 @@ function ISE(ElementSelector) {
 }
 
 //```````````````````````````````````````````````````````````````````````````````````````````````````````````\\
+// Source: https://stackoverflow.com/a/41491220/15715476
+// The name of the function, already explain it all
+function pickTextColorBasedOnBgColorSimple(bgColor, lightColor, darkColor) {
+  var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  var r = parseInt(color.substring(0, 2), 16); // hexToR
+  var g = parseInt(color.substring(2, 4), 16); // hexToG
+  var b = parseInt(color.substring(4, 6), 16); // hexToB
+  return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
+    darkColor : lightColor;
+}
+
+
+
+// Source: https://stackoverflow.com/a/3561711/15715476
+// Escape (with blackslash) special characters that have purpose to use in regex
+function escapeRegex(string) {
+    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
 //*/
