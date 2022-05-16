958b0264f108911ae1f7990b522549d3b53924ff
NJ <78418423+NJeyyy@users.noreply.github.com>
2022-05-16 21:57:56 +0700

    Fuck me dude.. Just an idiot *sigh

--- YT Scripts/YCS Addons/YCS AddonsEXTRA_FindCTimestamp.js
+++ YT Scripts/YCS Addons/YCS AddonsEXTRA_FindCTimestamp.js
@@ -195,7 +195,7 @@ async function FindComment_TimeStamps(e) {
 		ISE("#YCS_TimestampMatchResult label").removeAttribute('Loading');
 		ISE("#YCS_TimestampMatchResult label").removeAttribute('title');
 		ISE('#YCS_TimestampMatchResult .LoadingBar').removeAttribute('style');
-		if (SearchInput==document.querySelector("#ycs-input-search").value) {FindComment_TimeStamps();}
+		if (SearchInput!=document.querySelector("#ycs-input-search").value) {FindComment_TimeStamps();}
 	} else {console.error('BLOCKED!');}
 	return;
 }
