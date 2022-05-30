556f4816b7e567d11a44527abe5940cf26f21b83
NJ <78418423+NJeyyy@users.noreply.github.com>
2022-05-30 07:53:45 +0700

    add temporary solution for a bug

--- YT Scripts/YCS Addons/YCS AddonsEXTRA_FindCTimestamp.js
+++ YT Scripts/YCS Addons/YCS AddonsEXTRA_FindCTimestamp.js
@@ -195,9 +195,8 @@ async function FindComment_TimeStamps(e) {
 		ISE("#YCS_TimestampMatchResult label").removeAttribute('Loading');ISE('#YCS_TimestampMatchResult label ~ #LoadingBar').removeAttribute('title');
 		ISE("#YCS_TimestampMatchResult label").removeAttribute('Loading');
 		ISE('#YCS_TimestampMatchResult .LoadingBar').removeAttribute('style');
-		if (SearchInput!=document.querySelector("#ycs-input-search").value) {FindComment_TimeStamps();}else{
-			(Object.values($0.childNodes).filter(function(et){return et.nodeType == 3 && et.nodeValue.match(/^undefined$/i);})).forEach(function(that){that.remove();});
-		}
-	}/* else {console.error('BLOCKED!');}*/
+		if (SearchInput!=document.querySelector("#ycs-input-search").value) {FindComment_TimeStamps();}
+		setTimeout(function(){(Object.values($0.childNodes).filter(function(et){return et.nodeType == 3 && et.nodeValue.match(/^undefined$/i);})).forEach(function(that){that.remove();});}, 150);
+		}/* else {console.error('BLOCKED!');}*/
 	return;
 }
